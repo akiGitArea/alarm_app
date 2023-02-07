172d1dcd43a7b9db531216eb1536171990dc4a79
akiGitArea <akkuydmi@yahoo.co.jp>
2023-02-07 22:25:42 +0900

    first commit

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_app_badger
+import flutter_native_timezone
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterAppBadgerPlugin.register(with: registry.registrar(forPlugin: "FlutterAppBadgerPlugin"))
+  FlutterNativeTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterNativeTimezonePlugin"))
+}
